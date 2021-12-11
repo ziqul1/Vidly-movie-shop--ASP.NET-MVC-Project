32da612c0d8857c20850ac4054173924d7a8fb3c
Maciek <maciektaszarek@wp.pl>
2021-12-11 14:44:45 +0100

    Working section 1-3 and started section4

---
+++ Backup/VidlyBest/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyBest.Models;
+using System.Data.Entity;
+using System.Net;
+using VidlyBest.ViewModels;
+
+namespace VidlyBest.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        public ViewResult Index()
+        {
+            var customers = db.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = db.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+         
+        public ActionResult New()
+		{
+            var membershipTypes = db.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
+		}
+    }
+}
---
+++ Backup/VidlyBest/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyBest.Models;
+
+namespace VidlyBest.ViewModels
+{
+	public class NewCustomerViewModel
+	{
+		public IEnumerable<MembershipType> MembershipTypes { get; set; }
+		public Customer Customer { get; set; }
+
+
+	}
+}
