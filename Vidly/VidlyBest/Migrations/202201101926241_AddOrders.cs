0eb9311a08e24c501d56fcb9a6370f0bbafe580e
Maciek <maciektaszarek@wp.pl>
2022-01-10 21:01:18 +0100

    Added an Orders Table also Orders View

---
+++ Vidly/VidlyBest/Migrations/202201101926241_AddOrders.cs
@@ -0,0 +1,27 @@
+namespace VidlyBest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddOrders : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Products = c.String(),
+                        UserData = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Orders");
+        }
+    }
+}
