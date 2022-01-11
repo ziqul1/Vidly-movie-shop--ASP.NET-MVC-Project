dcd24da4f91339e751b0c3c6c7f85f5ae8e2ff2a
Maciek <maciektaszarek@wp.pl>
2022-01-11 11:19:21 +0100

    Added Cart View - working adding to cart

---
+++ Vidly/VidlyBest/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VidlyBest.Models;
+
+namespace VidlyBest.ViewModels
+{
+	public class ShoppingCartViewModel
+	{
+		//public Cart CartItems2 { get; set; }
+		public decimal CartTotal { get; set; }
+
+		public List<Cart> CartItems { get; set; }
+
+		//public Cart Cart { get; set; }
+
+
+		//[Key]
+		//public int RecordId { get; set; }
+		//public string CartId { get; set; }
+		//public int MovieId { get; set; }
+		//public int Count { get; set; }
+		//public System.DateTime DateCreated { get; set; }
+		//public virtual Movie Movie { get; set; }
+
+		//public ShoppingCartViewModel
+	}
+}
