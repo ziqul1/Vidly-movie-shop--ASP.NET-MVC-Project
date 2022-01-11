aa0af443ec75e75dc069ef2370f4c82ce21a8d6a
Maciek <maciektaszarek@wp.pl>
2022-01-11 12:12:38 +0100

    Fixed some bugs in Navbar

--- Vidly/VidlyBest/Controllers/HomeController.cs
+++ Vidly/VidlyBest/Controllers/HomeController.cs
@@ -14,6 +14,11 @@ namespace VidlyBest.Controllers
 			return View();
 		}
 
+		public ActionResult Index2()
+		{
+			return View();
+		}
+
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
